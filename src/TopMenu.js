0f3af88155d6e0dba6301135d7aad464d28071c2
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-06-06 20:42:11 +0100

    Update TopMenu.js padding

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const TopMenu = () => {
     return ( 
-        <section className="flex items-center justify-between max-w-xl px-10 my-8 mx-auto">
+        <section className="flex items-center justify-between max-w-xl px-4 my-8 mx-auto">
             <Link to='/' className="bg-black bg-opacity-25 px-4 py-2 rounded-3xl uppercase text-opacity-85 text-white">
                 Menu
             </Link>
